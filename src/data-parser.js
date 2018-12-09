ab5acd22a056cbd3826748655a2d1414a7dde2a4
idohalevi <ido.h@logz.io>
2018-12-09 11:19:43 +0200

    move back to for each

--- src/data-parser.js
+++ src/data-parser.js
@@ -1,40 +1,43 @@
 const isArray = arr => arr instanceof Array;
-const isEmptyArray = arr => isArray(arr) ? arr.length === 0 : false;
+const isEmptyArray = arr => (isArray(arr) ? arr.length === 0 : false);
 const isNil = item => item == null || item === 'null' || item === 'undefined';
 const isEmpty = item => item === '';
+const filterAllEmpty = (k, v) => !isEmpty(k) && !isNil(k) && !isEmpty(v) && !isNil(v) && !isEmptyArray(v);
 
 class DataParser {
-    constructor({
-        internalLogger = global.console
-    }) {
-        this.internalLogger = internalLogger;
+  constructor({
+    internalLogger = global.console,
+  }) {
+    this.internalLogger = internalLogger;
+  }
+
+  removeEmpty(obj) {
+    if (typeof (obj) === 'string') return obj; // for string event.
+
+    return Object.keys(obj)
+      .filter(k => filterAllEmpty(k, obj[k]))
+      .reduce((acc, k) => Object.assign(acc, {
+        [k]: typeof obj[k] === 'object' ? this.removeEmpty(obj[k]) : obj[k],
+      }), {});
+  }
+
+  pushParsedMsg(parsedMessages, msg) {
+    if (isArray(msg.records)) {
+      msg.records.forEach(subMsg => parsedMessages.push(this.removeEmpty(subMsg)));
+    } else {
+      parsedMessages.push(this.removeEmpty(msg));
     }
+  }
 
-    _removeEmpty(obj) {
-        if (typeof (obj) === 'string') return obj; //for string event.
-
-        return Object.keys(obj)
-            .filter(k => !isEmpty(k) && !isNil(k))
-            .filter(k => !isEmpty(obj[k]) && !isNil(obj[k]))
-            .filter(k => !isEmptyArray(obj[k]))
-            .reduce((acc, k) => Object.assign(acc, {
-                [k]: typeof obj[k] === 'object' ? this._removeEmpty(obj[k]) : obj[k]
-            }), {});
+  parseEventHubLogMessagesToArray(eventHubMessage) {
+    const parsedMessages = [];
+    if (isArray(eventHubMessage)) {
+      eventHubMessage.forEach(msg => this.pushParsedMsg(parsedMessages, msg));
+    } else {
+      this.pushParsedMsg(parsedMessages, eventHubMessage);
     }
-
-    _pushParsedMsg(parsedMessages, msg) {
-        isArray(msg.records) ?
-            msg.records.map(subMsg => parsedMessages.push(this._removeEmpty(subMsg))) :
-            parsedMessages.push(this._removeEmpty(msg));
-    }
-
-    parseEventHubLogMessagesToArray(eventHubMessage) {
-        const parsedMessages = [];
-        isArray(eventHubMessage) ?
-            eventHubMessage.map(msg => this._pushParsedMsg(parsedMessages, msg)) :
-            this._pushParsedMsg(parsedMessages, eventHubMessage);
-        return parsedMessages;
-    };
+    return parsedMessages;
+  }
 }
 
 module.exports = DataParser;
