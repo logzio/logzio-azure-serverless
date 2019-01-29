db4af90e15d981d638aaf43aaddf7b9b81115c10
AlonMiz <alon@logz.io>
2019-01-29 10:01:41 +0200

    eslint

---
+++ eventHubFunction/.eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    rules: {
+      "no-underscore-dangle": 0
+    },
+    'env': {
+        node: true,
+        es6: true,
+    },
+    'extends': [
+        'airbnb-base',
+    ],
+};
