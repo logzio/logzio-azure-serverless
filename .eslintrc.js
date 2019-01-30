4463e6ba72298cc69da5837858ee3c635f6cf66a
idohalevi <ido.h@logz.io>
2019-01-30 11:18:44 +0200

    fixing null/undifind

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    'rules': {
+        "no-underscore-dangle": 0
+    },
+    'env': {
+        node: true,
+        jest: true,
+        es6: true,
+    },
+    'extends': [
+        'airbnb-base',
+    ],
+};
---
+++ eventHubFunction/.eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+    rules: {
+        "no-underscore-dangle": 0,
+        "max-len": [1, {
+            code: 121
+        }],
+        "no-param-reassign": 0,
+    },
+    'env': {
+        node: true,
+        es6: true,
+    },
+    'extends': [
+        'airbnb-base',
+    ],
+}
