a65d9b6dcb8861425d297016885340f6b2ea31b9
ronish31 <roni.shaham@logz.io>
2021-07-16 10:42:24 +0300

    Revert "Support inner event (#53)"
    This reverts commit 137ea177fe75305a04d2b98b2c33446dac944e9d.

--- dist/logzioLogsFunction/core.js
+++ dist/logzioLogsFunction/core.js
@@ -134,15 +134,7 @@ module.exports = async function processEventHubMessages(context, eventHubs) {
   try{
       const eventHubArray = eventHubs[0].hasOwnProperty('records') ? eventHubs[0].records : eventHubs;
       eventHubArray.map(async eventHub => {
-        if (eventHub.hasOwnProperty('records')){
-          eventHub.records.map(async innerEventHub => {
-            sendLog(innerEventHub, logzioShipper, backupContainer, context);
-          })
-          await Promise.all(eventHub);
-        }
-        else{
           sendLog(eventHub, logzioShipper, backupContainer, context);
-        }
       });
       await Promise.all(eventHubArray);
       logzioShipper.sendAndClose(callBackFunction);
